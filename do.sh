f7d3edd4adbb21cc9243c674b0fb1173d19748cb
logan <logan@bytedance.com>
2021-11-16 14:56:55 +0800

    update do.sh

--- do.sh
+++ do.sh
@@ -5,6 +5,6 @@ dot_config_file_path=/lib/modules/`uname -r`/build/
 echo "reuse $dot_config_file_path/.config"
 cat $dot_config_file_path/.config | sed 's/^CONFIG_LOCALVERSION.*$/CONFIG_LOCALVERSION="hello"/' > .config
 make olddefconfig
-make -j8 bindeb-pkg
+make -j88 bindeb-pkg
 
 
