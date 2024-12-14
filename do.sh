4a11735198fd3518b2135f1c729eff2e68ec7317
anlaneg <anlaneg@126.com>
2024-12-14 19:33:58 +0800

    update do.sh

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-#apt install bison flex
-#apt install libssl-dev
+apt update
+apt install -y bison flex libssl-dev vim make linux-headers-`uname -r` dpkg-dev libelf-dev bc cpio kmod rsync debhelper zstd
 dot_config_file_path=/lib/modules/`uname -r`/build/
 echo "reuse $dot_config_file_path/.config"
 cat $dot_config_file_path/.config | sed 's/^CONFIG_LOCALVERSION.*$/CONFIG_LOCALVERSION="hello"/' > .config
